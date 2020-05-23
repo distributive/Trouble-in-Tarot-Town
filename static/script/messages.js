57152a4830db918d1dfee64bd46c48698d703a79
Amelie <github@ameliewd.com>
2020-05-23 23:42:08 +0100

    Message box now always jumps to bottom on receiving a new message

--- static/script/messages.js
+++ static/script/messages.js
@@ -1,28 +1,20 @@
 function receiveMessage (sender, content)
 {
-    // Check if scrolled to the bottom of the messages
-    let doScroll = $("#message-box")[0].scrollTop + $("#message-box").innerHeight () >= $("#message-box")[0].scrollHeight;
-
     // Add new message
     if (sender)
         $(messageTempl.replace ("@sender", sender).replace ("@content", content)).appendTo ($("#message-box"));
     else
         $(statementTempl.replace ("@content", content)).appendTo ($("#message-box"));
 
-    // If scrolled to the bottom of the messages, fix scroll height to the bottom of the messages
-    if (doScroll)
-        $("#message-box")[0].scrollTop = $("#message-box")[0].scrollHeight;
+    // Fix scroll height to the bottom of the messages
+    $("#message-box")[0].scrollTop = $("#message-box")[0].scrollHeight;
 }
 
 function splitMessages ()
 {
-    // Check if scrolled to the bottom of the messages
-    let doScroll = $("#message-box")[0].scrollTop + $("#message-box").innerHeight () >= $("#message-box")[0].scrollHeight;
-
     // Add new divider
     $("<hr>").appendTo ($("#message-box"));
 
-    // If scrolled to the bottom of the messages, fix scroll height to the bottom of the messages
-    if (doScroll)
-        $("#message-box")[0].scrollTop = $("#message-box")[0].scrollHeight;
+    // Fix scroll height to the bottom of the messages
+    $("#message-box")[0].scrollTop = $("#message-box")[0].scrollHeight;
 }
