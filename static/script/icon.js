2e1cd45e78975bd8395bd9311c36884170ff7585
Amelie <github@ameliewd.com>
2020-06-09 10:48:40 +0100

    Added animation to player's icon

--- static/script/icon.js
+++ static/script/icon.js
@@ -9,12 +9,36 @@ function setIcon (faction)
         url: `static/img/character/${faction}.png`,
         type: "HEAD",
         error: () => {
-            $("#player-faction").attr ("src", "");
-            $("#player-faction").hide ();
+            animateIcon (null);
         },
         success: () => {
-            $("#player-faction").attr ("src", `static/img/character/${faction}.png`);
-            $("#player-faction").show ();
+            animateIcon (`static/img/character/${faction}.png`);
         }
     });
+}
+
+function animateIcon (img)
+{
+    $("#player-faction").attr ("class", "");
+    $("#player-faction").css ("width", 0);
+    $("#player-faction div").css ("opacity", 1);
+
+    setTimeout (() => {
+        if (img == null)
+        {
+            $("#player-faction img").attr ("src", "");
+            $("#player-faction").hide ();
+        }
+        else
+        {
+            $("#player-faction img").attr ("src", img);
+            $("#player-faction").show ();
+        }
+
+        $("#player-faction").attr ("class", "flip");
+        $("#player-faction").css ("width", "100%");
+        $("#player-faction div").css ("opacity", 0);
+
+        setTimeout (() => {$("#player-faction").attr ("class", "");}, 1000);
+    }, 1000);
 }
